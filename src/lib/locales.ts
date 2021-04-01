c3fae07f9db1118d870b540c32bf01c8b41d566d
MehranTaheri <mehrant7510@gmail.com>
2021-04-01 13:11:56 +0430

    Add months

--- src/lib/locales.ts
+++ src/lib/locales.ts
@@ -1,12 +1,46 @@
+export type Locale = {
+  day: string;
+  month: string;
+  year: string;
+  months: string[];
+};
 export default {
   fa: {
     day: "روز",
     month: "ماه",
     year: "سال",
+    months: [
+      "فروردین",
+      "اردیبهشت",
+      "خرداد",
+      "تیر",
+      "مرداد",
+      "شهریور",
+      "مهر",
+      "آبان",
+      "آذر",
+      "دی",
+      "بهمن",
+      "اسفند",
+    ],
   },
   en: {
     day: "day",
     month: "month",
     year: "year",
+    months: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
   },
-};
+} as { [key: string]: Locale };
